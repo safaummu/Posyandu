91ad627e21e88fd2371e20d925406c8e36c6f8cd
safaummu <safaummunajwa_2428240131@mhs.mdp.ac.id>
2026-06-18 00:33:42 +0700

    fix: bungkus semua file dari root utama

--- api/index.php
+++ api/index.php
@@ -1,8 +1,8 @@
 <?php
 
-require __DIR__ . '/../posyandu/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../posyandu/bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
